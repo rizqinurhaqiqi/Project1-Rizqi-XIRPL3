53a424f54e22546f59b9137f0597993ae81db49e
rizqinurhaqiqi <rizqi7nurhaqiqi@gmail.com>
2021-05-29 10:53:22 +0700

    Implementasi Kelas Menu

--- AplikasiRestauran/src/classes/Menu.java
+++ AplikasiRestauran/src/classes/Menu.java
@@ -10,31 +10,34 @@ package classes;
  * @author Rizky
  */
 public class Menu {
-     private String nama_menu;
+    private String nama_menu;
     private double harga;
     private String kategori;
 
     public String getNama_menu(){
-        return "";
+         return nama_menu;
     }
 
     public void setNama_menu(String nama_menu){
 
+        this.nama_menu = nama_menu;
     }
 
     public double getHarga(){
-        return 0;
+        return harga;
     }
 
     void setHarga (double harga){
 
+        this.harga = harga;
     }
 
     public String getKategori(){
-        return "";
+        return kategori;
     }
 
     public void setKategori(String kategori){
 
+        this.kategori = kategori;
     }
 }
